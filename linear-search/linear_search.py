943aa746c6ddaf244113defac2d32d12f083687a
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-12-25 18:09:56 +0000

    UPDATED

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,12 +2,11 @@ def linear_search(lst, to_find):
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
-  # for i in range(len(lst)):
-    # if lst[i] == to_find:
-    #     return i
-  if to_find < len(lst):
-    return lst[to_find]
-  return -1
+  res = -1
+  for i in range(len(lst)):
+    if lst[i] == to_find:
+        res  = i
+  return res
     
 
 # print(linear_search([1, 2, 3, 4], 1))
