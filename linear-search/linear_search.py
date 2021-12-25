ca9461b34a38e665418ac0481367d68bd47b1149
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-12-25 18:02:44 +0000

    Update solution

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -2,11 +2,10 @@ def linear_search(lst, to_find):
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
-  res = -1
   for i in range(len(lst)):
     if lst[i] == to_find:
-        res = i
-  return res
+        return i
+  return -1
     
 
 # print(linear_search([1, 2, 3, 4], 1))
