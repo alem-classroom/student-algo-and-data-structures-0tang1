20d9169cef74c95cb7501a299378de9890601986
Tangzharyk <90594377+0tang1@users.noreply.github.com>
2021-12-25 18:28:17 +0000

    UPDATED Fibonacci

--- recursion/fib.py
+++ recursion/fib.py
@@ -1,9 +1,17 @@
 def fib(num):
     # return n-th Fibonacci number
-    if num == 1 or num == 2:
+    n1 = 0
+    n2 = 1
+    if num == 0:
+        return 0
+    elif num == 1 or num == 2:
         return 1
-    
-    return fib(num-1)+fib(num-2)
+    else:
+        for i in range(1, num):
+            n = n1 + n2
+            n1 = n2
+            n2 = n
+        return n2
 
 
 # print(fib(6))
